f82f65297c7a96c83d582e78be669c00392b7899
Alan Yeginchibayev <alayegin@WKMZTB5DBBB6.global.publicisgroupe.net>
2023-05-13 12:18:57 +0100

    final

--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/RestoreReadDbController.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/RestoreReadDbController.java
@@ -1,41 +1,43 @@
 package com.techbank.account.cmd.api.controllers;
 
-import com.techbank.account.cmd.api.commands.CloseAccountCommand;
+import com.techbank.account.cmd.api.commands.OpenAccountCommand;
+import com.techbank.account.cmd.api.commands.RestoreReadDbCommand;
 import com.techbank.account.cmd.api.dto.OpenAccountResponse;
 import com.techbank.account.common.dto.BaseResponse;
 import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.MessageFormat;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping(path = "/api/v1/closeBankAccount")
-public class CloseAccountController {
-    private final Logger logger = Logger.getLogger(WithdrawFundsController.class.getName());
+@RequestMapping(path = "/api/v1/restoreReadDb")
+public class RestoreReadDbController {
+    private final Logger logger = Logger.getLogger(RestoreReadDbController.class.getName());
 
     @Autowired
     private CommandDispatcher commandDispatcher;
 
-    @DeleteMapping(path = "/{id}")
-    public ResponseEntity<BaseResponse> closeAccount(@PathVariable(value="id") String id) {
-        try {
-            commandDispatcher.send(new CloseAccountCommand(id));
-            return new ResponseEntity<>(new BaseResponse("Bank account closure request successfully completed!"), HttpStatus.OK);
+    @PostMapping
+    public ResponseEntity<BaseResponse> restoreReadDb() {
+        try{
+            commandDispatcher.send(new RestoreReadDbCommand());
+            return new ResponseEntity<>(new BaseResponse("Restore request completed successfully"), HttpStatus.CREATED);
         } catch (IllegalStateException e) {
             logger.log(Level.WARNING, MessageFormat.format("Client made bad request - {0}.", e.toString()));
             return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            var safeErrorMessage = MessageFormat.format("Error while processing request to close a bank account for id - {0}", id);
+            var safeErrorMessage ="Error while processing request to open a new bank account for id - {0}";
             logger.log(Level.SEVERE, safeErrorMessage);
-            return new ResponseEntity<>(new OpenAccountResponse(safeErrorMessage, id), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new OpenAccountResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/domain/AccountAggregate.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/domain/AccountAggregate.java
@@ -15,6 +15,10 @@ public class AccountAggregate extends AggregateRoot {
     private Boolean active;
     private double balance;
 
+    public Boolean getActive(){
+        return this.active;
+    }
+
     public AccountAggregate(OpenAccountCommand command) {
         raiseEvent(AccountOpenedEvent.builder()
                     .id(command.getId())
